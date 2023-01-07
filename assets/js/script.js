dfd1158d1cd5e881c65ff5e3ef7cbfa844de03f3
mark279455 <mark279455@ntlworld.com>
2023-01-07 13:39:18 +0000

    add event listeners for doc load and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// Wait for the DOM to load before running game
+// Get button elements and add event listeners
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("submit");
+            }
+            else{
+                let gameType=this.getAttribute("data-type");
+                alert(gameType);
+            }
+        })
+    }
+})
+ 
 function runGame(){
 
 }
